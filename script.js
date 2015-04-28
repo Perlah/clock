dc0f021a0f99a11492ead1ea5cb2afc9fb85df5d
Perlah <herrp830@newschool.edu>
2015-04-28 18:08:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,109 @@
-/**
- * Here's where our JavaScript goes
- */
 
-// We still define our variables here
-var lastSecond;
+var button;
 var c;
+var c2;
+var c3;
+var song;
 
-/**
- * We're using the p5 "instance mode" for this new structure
- * http://p5js.org/learn/examples/Instance_Mode_Instantiation.php
- * So that we can use the function attribute `sketch` to access p5 functions
- */
-var mySketch = new p5(function(sketch) {
-
-
-	function getRandomColor() {
-		var r = sketch.random(0,255);
-		var g = sketch.random(0,255);
-		var b = sketch.random(0,255);
-
-		return sketch.color(
-			sketch.floor(r),
-			sketch.floor(g),
-			sketch.floor(b)
-		);
-	};
-
-
-	function mousePressed() {
-		var isFullscreen = sketch.fullscreen();
-		sketch.fullscreen(!isFullscreen);
-	}
-
-
-	function windowResized() {
-		sketch.resizeCanvas(sketch.windowWidth, sketch.windowHeight);
-	}
-
-	/**
+  /**
    * Same as `function setup() {}` before, but written a different way
    * using our namespace.
    */
-	sketch.setup = function() {
-		sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
-		lastSecond = sketch.second();
-		c = getRandomColor();
-	}
+function preload() {
+  song = loadSound('music/Fast & Furious 7.mp3');
+}
 
-	/**
-   * Same as `function setup() {}` before, but written a different way
+function mousePressed() {
+  if ( song.isPlaying() ) { // .isPlaying() returns a boolean
+    song.stop();
+  } else {
+    song.play();
+  }
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  // get things started
+
+    button = createButton('PLAY');
+    button.position(50, 65);
+    //button.mousePressed(); glitch?
+
+    c = color(255,0,0);
+    c2 = color(192, 0, 0);
+    c3 = color(128, 0, 0);
+
+    //song = loadSound('music/Fast & Furious 7.mp3');
+    //song.loop();
+
+    // create a new Amplitude analyzer
+    analyzer = new p5.Amplitude();
+    analyzer.setInput(song);
+
+    
+
+    //p.5 gibber code
+  //  createCanvas( windowWidth, windowHeight );
+
+  /*  drums = EDrums( 'x*o*x*o-' );
+
+    sampler = Sampler().record( drums, 1 )
+      .note.seq( [.25,.5,1,2].rnd(), [1/4,1/8,1/2].rnd() )
+      .fx.add( Delay(1/64))
+      .pan.seq( Rndf(-1,1) );
+
+    bass = Mono('bass')
+      .note.seq( [0,7], 1/8 );
+
+    Gibber.scale.root.seq( ['c4','eb4'], 1 );
+
+    follow = Follow( Gibber.Master, 1024 );
+
+    background( 64 );
+    noFill();
+    stroke( 10,0,0,127 );
+*/
+
+
+  }
+
+  /**
+   * Same as `function draw() {}` before, but written a different way
    * using our namespace.
    */
-	sketch.draw = function() {
-		sketch.background(0,0,0);
+function draw() {
+    // go!
 
-		if(sketch.lastSecond !== sketch.second()) {
-			sketch.lastSecond = sketch.second();
-			c = getRandomColor();
-		}
+    background(24, 0, 0);
+    fill(c);
+    var vol = analyzer.getLevel();
+    ellipse(windowWidth/5, windowHeight/1.5, 0+vol*300, 10+vol*300);
+    fill(c2);
+    ellipse(windowWidth/5, windowHeight/5, 0+vol*200, 10+vol*200);
+  //  fill(c3);
+    //ellipse(windowWidth/5, windowHeight/1.5, 0+vol*150, 10+vol*150);
 
-		sketch.fill(c);
-		// random rect
-		sketch.rect(
-			sketch.minute(),
-			sketch.second(),
-			sketch.day()+sketch.hour(),
-			sketch.month()+sketch.hour()
-		);
+
+  /*  //p5.gibber
+    var x = mouseX / windowWidth,
+        y = mouseY / windowHeight,
+        ww2 = windowWidth / 2,
+        wh2 = windowHeight / 2,
+        value = follow.getValue(),
+        radius = ( ww2 > wh2 ? wh2 : ww2 ) * value;
+
+    bass.resonance = (1 - x) * 5;
+    bass.cutoff = (1 - y) / 2;
+
+    sampler.fx[0].feedback = x < .99 ? x : .99;
+
+    strokeWeight( value * 50 );
+    background( 64,64,64,10 );
+    ellipse( ww2, wh2, radius, radius );
+*/
+  }
+
 
 		sketch.textSize(20);
 		sketch.fill(255,255,255);
